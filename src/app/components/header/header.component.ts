b7b20ad74c1facf77e79128eeb17644f31f9f7ae
kennjr <ken.juniour@student.moringaschool.com>
2022-02-04 15:11:44 +0300

    Created the fun for checking the current route

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  @Output() onAddQuote :EventEmitter<boolean> = new EventEmitter();
+
+  constructor(private router :Router) { }
+
+  ngOnInit(): void {
+  }
+
+  hasRoute (route :string){
+    return this.router.url === route;
+  }
+
+  notHasRoute (route :string){
+    return this.router.url != route;
+  }
+
+  addQuote(){
+    this.onAddQuote.emit(true);
+  }
+}
