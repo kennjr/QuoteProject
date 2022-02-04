312c6e87130ebfd7f89ee5934d7d56b9bc2b7a48
kennjr <ken.juniour@student.moringaschool.com>
2022-02-04 13:35:00 +0300

    Added the router and the first 2 paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QuotesListComponent } from './components/quotes-list/quotes-list.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes :Routes = [
+  {path:'', component:QuotesListComponent},
+  {path:'login', component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuotesListComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
