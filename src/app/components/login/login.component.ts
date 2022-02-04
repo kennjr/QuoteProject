7528e574132538295bd98c79b1dacdb12648dd67
kennjr <ken.juniour@student.moringaschool.com>
2022-02-04 20:04:36 +0300

    Added the logic for checking whether the user is logged in, in the ngOninit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,28 +14,59 @@ export class LoginComponent implements OnInit {
   email:string = "";
   password:string = "";
 
-  constructor() { }
+  currentLoginStatus = "false";
+
+  constructor(private authservice: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    
+
+    const status = this.checkLoginStatus().toString()
+      this.currentLoginStatus = status;
+      if(status == "true"){
+        console.log("The stat is true")
+        this.currentLoginStatus = "true";
+        if(confirm("You are logged in, would you like to navigate to the home page?")){
+          this.navigateToPath("/")
+        }
+      }
+      else if(status == "false"){
+        this.currentLoginStatus = "false";
+      }
   }
 
   loginAttempt(){
-    
-    let nameInput = this.name;
-    let emailInput = this.email;
-    let passwordInput = this.password;
+    if(this.currentLoginStatus == "false"){
+      let nameInput = this.name;
+      let emailInput = this.email;
+      let passwordInput = this.password;
+  
+      if(nameInput != "" && emailInput != "" && passwordInput != ""){
 
-    if(nameInput != "" && emailInput != "" && passwordInput != ""){
+        const user:User = {
+          name:nameInput,
+          email:emailInput,
+          password:passwordInput,
+          timestamp:new Date()
+        }
+        this.logUserIn(user)
 
-    }else if(nameInput == ""){
-      console.log("We're in the login attempt")
-      alert("The name field is empty");
-    }
-    else if(emailInput == ""){
-      alert("The email field is empty");
-    }
-    else if(passwordInput == ""){
-      alert("The password field is empty");
+        this.navigateToPath("/");
+  
+      }else if(nameInput == ""){
+        console.log("We're in the login attempt")
+        alert("The name field is empty");
+      }
+      else if(emailInput == ""){
+        alert("The email field is empty");
+      }
+      else if(passwordInput == ""){
+        alert("The password field is empty");
+      }
+    }else{
+      if(confirm("You are logged in, would you like to navigate to the home page?")){
+        this.navigateToPath("/")
+      }
     }
 
   }
@@ -43,4 +77,15 @@ export class LoginComponent implements OnInit {
     this.email="";
   }
 
+  navigateToPath (path :string){
+    this.router.navigateByUrl(path);
+  }
+
+  logUserIn (user :User){
+    this.authservice.addUser(user, true);
+  }
+
+  checkLoginStatus () :string{
+    return this.authservice.isLoggedIn
+  }
 }
