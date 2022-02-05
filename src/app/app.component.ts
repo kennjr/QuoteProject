4c7f3a4aadc7bbe335dbd77fdeac28b80a203ef4
kennjr <ken.juniour@student.moringaschool.com>
2022-02-05 09:42:45 +0300

    Added the fun. that adds a quote to the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Quote } from './models/Quote';
+import { User } from './models/User';
+import { AuthService } from './services/auth.service';
+import { QuotesService } from './services/quotes.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QuoteProject';
+
+  userInfo! :User;
+  username :string = "";
+
+  quotes:Quote[] = [];
+
+  constructor (private router:Router, private authservice: AuthService, private quoteservice: QuotesService){}
+
+  addNewNoteRequest = false;
+
+  newQuote(status: boolean){
+    this.addNewNoteRequest = status;
+  }
+
+  hasRoute (route :string){
+    return this.router.url === route;
+  }
+
+  addANewQuote(quote :Quote){
+    if (this.username == ""){
+      this.getUserDetails();
+    }
+
+    quote.creator = this.username;
+    this.addNewQuoteToDB(quote);
+  }
+
+  getUserDetails(){
+    this.userInfo = this.authservice.userDetails;
+    this.username = this.userInfo.name;
+  }
+
+  // The fun. that communicates with the server
+  addNewQuoteToDB (quote: Quote){
+    // console.log(task)
+    this.quoteservice.addQuote(quote).subscribe((quote) => (this.quotes.unshift(quote)))
+    console.log(this.quotes);
+  }
 }
