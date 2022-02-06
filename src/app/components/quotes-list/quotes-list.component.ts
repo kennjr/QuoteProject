bf722e81a2297aba16b7e34dbf9d5a6216e5cb8e
kennjr <ken.juniour@student.moringaschool.com>
2022-02-06 14:55:16 +0300

    Added the fun for toggling the viewof a single quote

--- src/app/components/quotes-list/quotes-list.component.ts
+++ src/app/components/quotes-list/quotes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/models/User';
 import { Quote } from 'src/app/models/Quote';
 import { AuthService } from 'src/app/services/auth.service';
@@ -10,17 +10,21 @@ import { QuotesService } from 'src/app/services/quotes.service';
   styleUrls: ['./quotes-list.component.css']
 })
 export class QuotesListComponent implements OnInit {
+  @Input() quotesFromParent!:Quote[] ;
 
   details!:User;
   teststring:string="";
 
   allQuotes:Quote[] = [] ;
+  allQuotesArray:Quote[] = [];
 
   constructor(private authservice :AuthService, private quoteservice :QuotesService) { }
 
   ngOnInit(): void {
+    this.allQuotes = this.quotesFromParent
     this.getUserDetails ();
     this.getAllQuotes ();
+    this.getLocalQuotesArray();
   }
 
   getUserDetails(){
@@ -30,7 +34,24 @@ export class QuotesListComponent implements OnInit {
 
   // The fun that'll get us all the quotes from the server
   getAllQuotes (){
-    this.quoteservice.getQuotes().subscribe((quotes) => (this.allQuotes = quotes))
+    this.quoteservice.getQuotes().subscribe((quotes) => {
+      this.allQuotesArray.push(...quotes)
+      this.allQuotes = quotes
+    })
+  }
+
+  getLocalQuotesArray(){
+    this.quoteservice.getQuotesArray().subscribe((quotesArray) => (this.allQuotesArray = quotesArray))
+  }
+
+  toggleQuoteDetails (index: number){
+    this.allQuotesArray[index].expandView = !this.allQuotesArray[index].expandView
+  }
+
+  updateVoteCount (index: number, isUpVt: boolean){
+    const quote = this.allQuotesArray[index];
+
+    console.log("The quote " + quote.quote + " has a new " + isUpVt)
   }
 
 }
