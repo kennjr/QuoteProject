45218105ef4512c43fa3e7476d97378217115cb8
kennjr <ken.juniour@student.moringaschool.com>
2022-02-07 08:36:11 +0300

    Changed the fun for updating the vote count

--- src/app/components/quotes-list/quotes-list.component.ts
+++ src/app/components/quotes-list/quotes-list.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/models/User';
 import { Quote } from 'src/app/models/Quote';
 import { AuthService } from 'src/app/services/auth.service';
 import { QuotesService } from 'src/app/services/quotes.service';
+import { VoteRequest } from 'src/app/models/VoteRequest';
 
 @Component({
   selector: 'app-quotes-list',
@@ -10,7 +11,6 @@ import { QuotesService } from 'src/app/services/quotes.service';
   styleUrls: ['./quotes-list.component.css']
 })
 export class QuotesListComponent implements OnInit {
-  @Input() quotesFromParent!:Quote[] ;
 
   details!:User;
   teststring:string="";
@@ -21,11 +21,12 @@ export class QuotesListComponent implements OnInit {
   constructor(private authservice :AuthService, private quoteservice :QuotesService) { }
 
   ngOnInit(): void {
-    this.allQuotes = this.quotesFromParent
-    this.getUserDetails ();
     this.getAllQuotes ();
     this.getLocalQuotesArray();
+    this.getUserDetails ();
   }
+
+  mostUpvotedQuoteIndex:number = 0
 
   getUserDetails(){
     this.details = this.authservice.userDetails
@@ -36,47 +37,74 @@ export class QuotesListComponent implements OnInit {
   getAllQuotes (){
     this.quoteservice.getQuotes().subscribe((quotes) => {
       this.allQuotesArray.push(...quotes)
+      this.mostUpvotedQuoteIndex = this.quoteservice.getQuoteWithHighestUpVote();
       this.allQuotes = quotes
     })
   }
 
   getLocalQuotesArray(){
-    this.quoteservice.getQuotesArray().subscribe((quotesArray) => (this.allQuotesArray = quotesArray))
+    this.quoteservice.getQuotesArray().subscribe((quotesArray) => {
+      this.allQuotesArray = quotesArray
+      this.mostUpvotedQuoteIndex = this.quoteservice.getQuoteWithHighestUpVote();
+    })
   }
 
   toggleQuoteDetails (index: number){
     this.allQuotesArray[index].expandView = !this.allQuotesArray[index].expandView
   }
 
-  updateVoteCount (index: number, isUpVt: boolean){
+  // Update the num. of votes for an item based on the clicked btn
+  updateVoteCount (voteRequest: VoteRequest){
+    let index: number = voteRequest.voteIndex 
+    let isUpVt: boolean = voteRequest.isUpVt!
     let quote = this.allQuotesArray[index];
     if(isUpVt && !quote.isUpVt && !quote.isDwnVt){
       quote.upvt_count += 1;
       quote.isUpVt = true;
-      console.log("The first")
     }
     else if(!isUpVt && !quote.isDwnVt && !quote.isUpVt){
       quote.dwnvt_count += 1;
       quote.isDwnVt = true;
-      console.log("The second")
     }
-    else if(isUpVt && quote.isDwnVt){
+    else if(isUpVt && quote.isDwnVt && !quote.isUpVt){
       quote.dwnvt_count -= 1;
       quote.upvt_count += 1;
 
       quote.isUpVt = true;
       quote.isDwnVt = false;
-      console.log("The third")
     }
-    else if(!isUpVt && quote.isUpVt){
+    // If is't a dwv-vt request and the user had already added an up-vote
+    else if(!isUpVt && quote.isUpVt && !quote.isDwnVt){
       quote.dwnvt_count += 1;
       quote.upvt_count -= 1;
 
       quote.isUpVt = false;
       quote.isDwnVt = true;
-      console.log("The fourth")
+    }
+    // If the user added dwn votes and the dwn vote had been added then we just subtract the added dwn-vote
+    else if(!isUpVt && quote.isDwnVt && !quote.isUpVt){
+      quote.dwnvt_count -= 1;
+      quote.upvt_count -= 1;
+
+      quote.isUpVt = false;
+      quote.isDwnVt = false;
+    }
+    // If the user added up votes and the up vote had been added then we just subtract the added up-vote
+    else if(isUpVt && quote.isUpVt && !quote.isDwnVt){
+      quote.dwnvt_count -= 1;
+      quote.upvt_count -= 1;
+
+      quote.isUpVt = false;
+      quote.isDwnVt = false;
     }
     this.quoteservice.updateLocalArrayQuote(index, quote);
+  }
+
+  
+
+  ngAfterViewInit(): void {
+    console.log("The message ran")
+    this.mostUpvotedQuoteIndex = this.quoteservice.getQuoteWithHighestUpVote()
   }
 
 }
