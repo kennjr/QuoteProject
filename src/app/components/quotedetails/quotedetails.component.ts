6b7737ab2fb71347bc38f03cd104a3182793dcaa
kennjr <ken.juniour@student.moringaschool.com>
2022-02-07 12:03:27 +0300

    Added the eventemitter that'll send a click whenver the user clicks on the quote for it to collapse

--- src/app/components/quotedetails/quotedetails.component.ts
+++ src/app/components/quotedetails/quotedetails.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from 'src/app/models/Quote';
 import { VoteRequest } from 'src/app/models/VoteRequest'
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-quotedetails',
@@ -11,12 +12,34 @@ export class QuotedetailsComponent implements OnInit {
 
   @Input() quote?: Quote;
   @Input() index?: number;
+  @Input() username?: string;
+
+  currentName!:string;
+
 
   @Output() onUpdateVoteCount :EventEmitter<VoteRequest> = new EventEmitter()
+  @Output() onToggleQuoteView :EventEmitter<number> = new EventEmitter ()
 
-  constructor() { }
+  constructor(private authservice: AuthService) { }
 
   ngOnInit(): void {
+    this.getUserDetails()
+  }
+
+  showQuoteDetails (){
+    this.onToggleQuoteView.emit(this.index)
+  }
+
+  getUserDetails(){
+    
+    let theName = this.authservice.userDetails;
+    if(theName == false){
+      console.log("The name" + theName)
+      this.currentName = "Anonymous";
+    }else{
+      console.log("The name" + theName)
+      this.currentName = theName.name;
+    }
   }
 
   sendVoteCountUpdate (isUpVt: boolean){
