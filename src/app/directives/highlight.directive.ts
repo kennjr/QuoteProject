604acf0725f5a77394f95a1cba1ac6d48f113cbf
kennjr <ken.juniour@student.moringaschool.com>
2022-02-07 08:06:50 +0300

    Changed the color of the text of a highlighted quote item

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+
   constructor(private elementRef: ElementRef) { 
-    this.bgColor();
+    this.bgColor()
+    
   }
 
   private bgColor (){
-    this.elementRef.nativeElement.style.backgroundColor = 'orange';
+    this.elementRef.nativeElement.style.backgroundColor = '#ff5722';
+    this.elementRef.nativeElement.style.color = '#fff';
   }
 
 }
